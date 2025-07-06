c3b3943b19fabbbb3495cadfd65ef79bac1c4ebf
Wilmer Arambula <terabytesoftw@gmail.com>
2025-07-06 14:53:33 -0400

    feat: add `Oracle` database support with corresponding test classes for improved functionality.

---
+++ tests/oracle/ExceptionHandlingTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yii2\extensions\nestedsets\tests\oracle;
+
+use PHPUnit\Framework\Attributes\Group;
+use yii2\extensions\nestedsets\tests\base\AbstractExceptionHandling;
+
+#[Group('oci')]
+final class ExceptionHandlingTest extends AbstractExceptionHandling
+{
+    protected string $driverName = 'oci';
+    protected string|null $dsn = 'oci:dbname=localhost/XE;charset=AL32UTF8;';
+    protected string $password = 'root';
+    protected string $username = 'system';
+}
