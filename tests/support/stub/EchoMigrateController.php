ac460f55fb685503261185f995c4f52e3a3b0c70
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2025-07-07 08:42:34 -0400

    feat: Add migration files for `tree` and `multiple_tree` tables with support for various database drivers. (#66)

---
+++ tests/support/stub/EchoMigrateController.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yii2\extensions\nestedsets\tests\support\stub;
+
+use yii\console\controllers\MigrateController;
+
+final class EchoMigrateController extends MigrateController
+{
+    public function stdout($string)
+    {
+        echo $string;
+
+        return true;
+    }
+}
